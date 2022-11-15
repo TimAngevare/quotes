bfc2a70e1c548f945dbde40748c2db0bb1b8fd73
Tim <t.p.angevare@student.utwente.nl>
2022-11-15 23:10:30 +0100

    Cleanup

--- src/Firebase.js
+++ src/Firebase.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBdqkWySQ5lH5UHA1zXODQWnQ-J8hwkmpc",
-  authDomain: "quotes-2e087.firebaseapp.com",
-  projectId: "quotes-2e087",
-  storageBucket: "quotes-2e087.appspot.com",
-  messagingSenderId: "327524409869",
-  appId: "1:327524409869:web:302c987faf13d0bba0ab44",
-  measurementId: "G-X2GHHR5MRV"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
 };
 
 // Initialize Firebase
@@ -21,3 +21,4 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
+export const auth = app.auth();
